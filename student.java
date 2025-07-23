069b54ed219bf4b71b31cd22696c17d00cf08ec5
AryanParikh31 <aryanparikh22@gmail.com>
2025-07-23 10:32:03 +0530

    student.java

--- student.java
+++ student.java
@@ -19,7 +19,7 @@ class studentinfo{
 
     }
 }
-public class student{
+class student{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter endrollnment number: ");
